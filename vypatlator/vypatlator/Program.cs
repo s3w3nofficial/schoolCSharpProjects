bc6ba67720f9e9b89ee77d0ecca94e3732146f6a
Martin Urbanec <martin@urbanec.cz>
2017-01-16 12:21:08 +0100

    +vypatlator

---
+++ vypatlator/vypatlator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace vypatlator
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//string s = Console.ReadLine ().ToLower();
+			string s = "Lorem ipsum dolor sit amet constructor s w".ToLower();
+			string puvs = s;
+			string res = "";
+			Random r = new Random ();
+			for (int i = 0; i < s.Length; i++) {
+				int n = r.Next (0, 2);
+				if (n == 1) {
+					res += s [i].ToString ().ToUpper();
+				} else {
+					res += s [i].ToString ();
+				}
+			}
+			s = res;
+			res = "";
+			for (int i = 0; i < s.Length; i++) {
+				
+			}
+			Console.WriteLine (puvs);
+			Console.WriteLine (res);
+		}
+	}
+}
